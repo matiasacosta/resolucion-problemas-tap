b3b050985aa9c66c73990d8ffa7144df4017dabb
Matias <matiasacosta1995@gmail.com>
2019-10-04 13:35:09 -0300

    Fix funcion

--- 2018/a_guardar/AcostaMatias/main.py
+++ 2018/a_guardar/AcostaMatias/main.py
@@ -6,9 +6,9 @@ if __name__ == "__main__":
     print(caja.get_posicion_inicial())
 
     def descubir_caminos(posicion):
-    for movimiento in ['U', 'D', 'L', 'R']:
-        nueva_posicion = moverme(movimiento, posicion)
-        if nueva_posicion and nueva_posicion not in vertices:
-            vertices.append(nueva_posicion)
-            descubir_caminos(nueva_posicion)
+        for movimiento in ['U', 'D', 'L', 'R']:
+            nueva_posicion = moverme(movimiento, posicion)
+            if nueva_posicion and nueva_posicion not in vertices:
+                vertices.append(nueva_posicion)
+                descubir_caminos(nueva_posicion)
     
