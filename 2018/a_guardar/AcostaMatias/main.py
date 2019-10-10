6c55d6a08b3c9f112e24122c05c959c8cb244edb
Matias <matiasacosta1995@gmail.com>
2019-10-10 10:29:46 -0300

    Probar con la solucion 2

--- 2018/a_guardar/AcostaMatias/main.py
+++ 2018/a_guardar/AcostaMatias/main.py
@@ -2,13 +2,13 @@ from Caja import Caja
 
 if __name__ == "__main__":
     # Controlamos la entrada de datos
-    caja = Caja(['#.##', '..##', '..##', '.E.C'])
+    caja = Caja(['........', '..C..E..', '........', '........'])
     vertices = []
     caja.descubrir_caminos(caja.posicion_actual, vertices)
     print(vertices)
     for vertice in caja.grafo:
         print(f"Soy el vertice {vertice} y estoy conectado a " + " ".join(str(v) for v in vertice.obtener_conexiones()))
     print(caja.grafo.dijkstra(
-        (3,3,'-','-'),
-        (3,1,'-','-')
+        (1,2,'-','-'),
+        (1,5,'-','-')
     ))
